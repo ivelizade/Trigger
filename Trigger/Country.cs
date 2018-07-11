2fa7653bff36e0bde4cf04c1f4a5e6a613f61e7e
Velizadeh Ibrahim <ivelizade@outlook.com>
2018-07-11 21:51:24 +0400

    Add project files.

---
+++ Trigger/Country.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trigger
+{
+   public class Country
+    {
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+
+        public Country(string name, string shortname)
+        {
+            Name = name;
+            ShortName = shortname;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Trigger/Coutries.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trigger
+{
+    public class Countries : ObservableCollection<Country>
+    {
+        public Countries()
+        {
+            this.Add(new Country("Ukraine", "UA"));
+            this.Add(new Country("Russia", "RU"));
+            this.Add(new Country("United States Of America", "USA"));
+            this.Add(new Country("Azerbaijan", "AZ"));
+            this.Add(new Country("United Kingdom", "UK"));
+            this.Add(new Country("Turkey", "TR"));
+        }
+      
+    }
+}
